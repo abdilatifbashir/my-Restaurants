db8695b4c8a79faa9274246752f61ef5bb022478
abdilatifbashir <abdiladifbashir0000@gmail.com>
2018-01-18 11:43:21 +0300

    add input field

--- app/src/main/java/com/github/abdilatif/myrestaurants/MainActivity.java
+++ app/src/main/java/com/github/abdilatif/myrestaurants/MainActivity.java
@@ -1,13 +1,39 @@
 package com.github.abdilatif.myrestaurants;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private Button mFindRestaurantsButton;
+    private EditText mLocationEditText;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              /*  Toast.makeText(MainActivity.this, "cool you will recieve hotels around you soon!", Toast.LENGTH_LONG).show();*/
+                String location = mLocationEditText.getText().toString();
+                Log.e(TAG, location);
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+}
 }
